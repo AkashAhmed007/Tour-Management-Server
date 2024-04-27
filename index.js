05a0b9b8ffe06804c908357becd2a4f13b62f7dc
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-04-27 21:56:24 +0600

    find spot by specific id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middleware
@@ -35,14 +35,18 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
-
-
+    app.get('/addspot/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await addSpot.findOne(query)
+      res.send(result)
+    })
     app.post('/addspot', async(req,res)=>{
       const addspot = req.body
-      console.log(addspot)
       const result = await addSpot.insertOne(addspot)
       res.send(result)
     })
+
 
 
 
